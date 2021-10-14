4a29f90b73a5817a02e56403e89b89df9bce0371
Shahriar Saleque <salequeshahriar@gmail.com>
2021-10-14 16:16:22 +0600

    setup routes, add middleware and connect to DB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,32 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import postRouter from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRouter);
+
+// setup bodyparser middleware and limit increase for image sending
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// setup cors middleware
+app.use(cors());
+
+// connect to remote mongoDB
+const CONNECTION_URL =
+  "mongodb+srv://Shahriar:shahriar5678@cluster0.ptlyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port ${port}`))
+  )
+  .catch((err) => console.log(err.message));
